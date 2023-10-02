0b4c99db367a47683e263129f55f19f411de9791
ironsummit72 <ironsumit72@gmail.com>
2023-10-02 14:19:20 +0530

    header design complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import "./header.css"
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+   <>
+<div className="header">
+  <Link to={"/logo"} className='logo'>
+    <img src='./logo/companylogo.png' alt='companyLogo' className='logo-img'></img>
+    <h2 className="companyName">FruityBay</h2>
+  </Link>
+  <ul className='nav-link-list'>
+    <li>
+      <Link className='nav-links' to={'/'}>Home</Link>
+    </li>
+    <li>
+      <Link className='nav-links' to={'/about'}>About</Link>
+    </li>
+    <li>
+      <Link className='nav-links' to={'/contact'}>Contact</Link>
+    </li>
+    <li>
+      <Link className='nav-links' to={'/github'}>Github</Link>
+    </li>
+  </ul>
+  <div className="buttons">
+    <Link className='login' to={'/login'}>Log in</Link>
+    <Link className='getstarted' to={'/getstarted'}>Get started</Link>
+  </div>
+</div>
+   </>
   )
 }
 
