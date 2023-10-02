c7b62de76c50a15005b5085c09eb81f098b801af
ironsummit72 <ironsumit72@gmail.com>
2023-10-02 13:26:52 +0530

    added exports.js for easier imports of components

---
+++ src/export.js
@@ -0,0 +1,8 @@
+import Home from "./components/home/Home"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import About from "./components/about/About"
+import Github from "./components/github/Github"
+import Contact from "./components/Contact/Contact"
+
+export {Home,Header,Footer,About,Github,Contact}
