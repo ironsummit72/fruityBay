c30a231eb633179a6bebf22c41fd77662b30953f
ironsummit72 <ironsumit72@gmail.com>
2023-10-03 12:57:58 +0530

    website under development page added

--- src/export.js
+++ src/export.js
@@ -4,5 +4,6 @@ import Footer from "./components/footer/Footer"
 import About from "./components/about/About"
 import Github from "./components/github/Github"
 import Contact from "./components/Contact/Contact"
+import Underdevelopment from "./components/development/Underdevelopment"
 
-export {Home,Header,Footer,About,Github,Contact}
+export {Home,Header,Footer,About,Github,Contact,Underdevelopment}
